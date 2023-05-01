0ef3d35df9456badffef4c31d4d7f387b7f8ecb5
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-01 19:59:43 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* This SQL script populates the 'animals' table with sample data for testing and demonstration purposes. */
-INSERT INTO animals VALUES (1,'Agumon', 'Feb 3, 2020', 0, TRUE, 10.23);
-INSERT INTO animals VALUES (2,'Gabumon', 'Nov 15, 2018', 2, TRUE, 8);
-INSERT INTO animals VALUES (3,'Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04);
-INSERT INTO animals VALUES (4,'Devimon', 'May 12, 2017', 5, TRUE, 11);
+INSERT INTO animals VALUES (1, 'Agumon', 'Feb 3, 2020', 0, TRUE, 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon', 'Nov 15, 2018', 2, TRUE, 8);
+INSERT INTO animals VALUES (3, 'Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', 'May 12, 2017', 5, TRUE, 11);
