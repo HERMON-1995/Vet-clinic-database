005f598736fa6e7996b50628750c981f2be8bc3c
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-01 19:59:52 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2020';
-SELECT * FROM animals WHERE neutered IS TRUE AND escape_attempts < 3;
+SELECT name FROM animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2019';
+SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered IS TRUE;
