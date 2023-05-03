f245967b9037a6bc8d175e5eea2f8cef9d1b54fb
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-03 16:43:57 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,3 +12,43 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(150);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(150),
+    age INT
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150)
+);
+
+CREATE SEQUENCE IF NOT EXISTS animals_id_seq;
+SELECT SETVAL('animals_id_seq',(SELECT max(id) FROM animals));
+ALTER TABLE animals
+    ALTER COLUMN id
+    SET DEFAULT nextval('animals_id_seq'::regclass);
+ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+    ADD COLUMN IF NOT EXISTS species_id INT;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+    ADD COLUMN IF NOT EXISTS owner_id INT;
+    
+ALTER TABLE animals
+    ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
